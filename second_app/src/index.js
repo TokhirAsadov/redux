0106e2cb238d1f8a5876f7a502a2ddd173b30681
Tohir <guvalakat1603@gmail.com>
2022-07-04 17:04:32 +0500

    don`t use react

--- second_app/src/index.js
+++ second_app/src/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createStore } from "redux";
+
+const intialState = 0;
+
+const reducer = (state = intialState, action) => {
+  switch (action.type){
+    case "INCREMENT": {
+      return state + 1;
+    }
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+store.subscribe(()=>{ // kuzatuvchi uzgarishni tutib oladi
+  console.log(store.getState())
+})
+store.dispatch({type:"INCREMENT"});
+store.dispatch({type:"INCREMENT"});
+store.dispatch({type:"INCREMENT"});
+
+
+
+// let state = reducer(intialState,{ type: "INCREMENT" });
+// state = reducer(intialState,{ type: "INCREMENT" });
+// state = reducer(intialState,{ type: "INCREMENT" });
+// state = reducer(intialState,{ type: "INCREMENT" });
+// state = reducer(intialState,{ type: "INCREMENT" });
+// console.log(state);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <></>
   </React.StrictMode>
 );
 
