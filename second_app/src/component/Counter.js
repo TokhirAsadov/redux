5c34b5f5a6989274dfd78e3dbadf72c2905caf96
Tohir <guvalakat1603@gmail.com>
2022-07-18 20:23:33 +0500

    use connect for class component

--- second_app/src/component/Counter.js
+++ second_app/src/component/Counter.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React,{Component} from 'react';
 import {connect} from "react-redux";
 import * as actions from "../redux/actions";
 import {bindActionCreators} from "redux";
 
 
+
+
 /** ================   Functional Component   =================== **/
-// function Counter({ counter, inc, dec, random }) {
+// function Counter({ counter, inc, dec, random }) { // connect(mapStateToProps,actions)(Counter) <- props lar connect function dan kelyapdi
 //   return (
 //     <div>
 //       <h1>Counter: {counter}</h1>
@@ -15,6 +17,22 @@ import {bindActionCreators} from "redux";
 //     </div>
 //   );
 // }
+
+/** ================   Class Component   =================== **/
+class Counter extends Component {
+  render() {
+    const {counter,inc,dec,random} = this.props // connect(mapStateToProps,actions)(Counter) <- props lar connect function dan kelyapdi
+
+    return (
+        <div>
+          <h1>Counter: {counter}</h1>
+          <button onClick={inc} className="btn btn-primary">Increment +</button>
+          <button onClick={dec} className="btn btn-danger">Decrement -</button>
+          <button onClick={random} className="btn btn-info">Random#</button>
+        </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
