97a3b48cc9f5814d680ed58ceff89c366dc9dfb3
Tohir <guvalakat1603@gmail.com>
2022-07-18 15:35:37 +0500

    use actions and reducer

---
+++ second_app/src/redux/reducer.js
@@ -0,0 +1,28 @@
+const initialState = { value: 0 };
+
+const reducer = (state = initialState, action) => {
+  switch (action.type){
+    case "INCREMENT": {
+      return {
+        ...state,
+        value: state.value + 1,
+      };
+    }
+    case "DECREMENT": {
+      return {
+        ...state,
+        value: state.value - 1,
+      };
+    }
+    case "RANDOM": {
+      return {
+        ...state,
+        value: action.payload,
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+export default reducer;
