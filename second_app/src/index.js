b62d248399a7780bee24ac8012f872cd7cec2384
Tohir <guvalakat1603@gmail.com>
2022-07-19 17:08:54 +0500

    add window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() redux devtools

--- second_app/src/index.js
+++ second_app/src/index.js
@@ -5,7 +5,7 @@ import reducer from "./redux/reducer";
 import {Provider} from "react-redux";
 import App from "./component/App";
 
-const store = createStore(reducer);
+const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
