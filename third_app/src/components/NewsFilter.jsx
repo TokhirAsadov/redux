732b4e03206648ae5d2d5c8864d3afccb4fe7be3
Tohir <guvalakat1603@gmail.com>
2022-07-24 19:24:36 +0500

    add styles

--- third_app/src/components/NewsFilter.jsx
+++ third_app/src/components/NewsFilter.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 function NewsFilter(props) {
   return (
-    <div>NewsFilter</div>
+    <div className="card shadow-lg mt-4">
+      <div className="card-body">
+        <p className="card-text">Filter by category</p>
+        <div className="btn-group">
+          <button className="btn btn-outline-dark active">All</button>
+          <button className="btn btn-outline-danger">Hot News</button>
+          <button className="btn btn-outline-primary">Sport News</button>
+          <button className="btn btn-outline-success">World News</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
