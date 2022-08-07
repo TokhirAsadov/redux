747df9a8e93a85aaadac87f5dc404287704d9323
Tohir <guvalakat1603@gmail.com>
2022-08-07 22:18:47 +0500

    use createReducer function from redux-toolkit second view

--- third_app/src/redux/reducers/news.js
+++ third_app/src/redux/reducers/news.js
@@ -6,19 +6,31 @@ const initialState = {
   newsLoadingStatus: "tohir",
 }
 
-const news = createReducer(initialState, builder => {
-  builder
-    .addCase(newsFetching,(state) => {state.newsLoadingStatus = 'loading';})
-    .addCase(newsFetched,(state,action) => {
+const news = createReducer(initialState,{
+  [newsFetching]: state => {state.newsLoadingStatus = 'loading'},
+  [newsFetched]: (state,action) => {
       state.newsLoadingStatus = 'tohir';
       state.news = action.payload;
-    })
-    .addCase(newsFetchingError,state => {state.newsLoadingStatus = "error"})
-    .addCase(newsCreated, (state,action) => {state.news.push(action.payload)})
-    .addCase(newsDeleted,(state,action) => {state.news = state.news.filter(s => s.id !== action.payload)})
-    .addDefaultCase(() => {})
-});
-export default news;
+    },
+  [newsFetchingError]: state => {state.newsLoadingStatus = "error"},
+  [newsCreated]: (state,action) => {state.news.push(action.payload)},
+  [newsDeleted]: (state,action) => {state.news = state.news.filter(s => s.id !== action.payload)}
+},[],state =>state)
+
+// const news = createReducer(initialState, builder => {
+//   builder
+//     .addCase(newsFetching,(state) => {state.newsLoadingStatus = 'loading';})
+//     .addCase(newsFetched,(state,action) => {
+//       state.newsLoadingStatus = 'tohir';
+//       state.news = action.payload;
+//     })
+//     .addCase(newsFetchingError,state => {state.newsLoadingStatus = "error"})
+//     .addCase(newsCreated, (state,action) => {state.news.push(action.payload)})
+//     .addCase(newsDeleted,(state,action) => {state.news = state.news.filter(s => s.id !== action.payload)})
+//     .addDefaultCase(() => {})
+// });
+
+
 // const news = (state = initialState,action) => {
 //   switch (action.type){
 //     case "NEWS_FETCHING":
@@ -53,4 +65,5 @@ export default news;
 //       return state;
 //   }
 // }
-// export default news;
+
+export default news;
