924e70d135c3d1877ff8d1e01479b850fb93cca0
Tohir <guvalakat1603@gmail.com>
2022-08-07 22:10:05 +0500

    use createReducer function from redux-toolkit

--- third_app/src/redux/reducers/news.js
+++ third_app/src/redux/reducers/news.js
@@ -1,39 +1,56 @@
+import { createReducer } from "@reduxjs/toolkit";
+import { newsFetching,newsFetched,newsFetchingError,newsCreated,newsDeleted } from "../actions";
+
 const initialState = {
   news: [],
   newsLoadingStatus: "tohir",
 }
-const news = (state = initialState,action) => {
-  switch (action.type){
-    case "NEWS_FETCHING":
-      return {
-        ...state,
-        newsLoadingStatus: 'loading'
-      }
-    case "NEWS_FETCHED":
-      return {
-        ...state,
-        news: action.payload,
-        newsLoadingStatus: "tohir",
-      }
-    case "NEWS_FETCHING_ERROR":
-      return {
-        ...state,
-        newsLoadingStatus: 'error'
-      }
-    case "NEWS_CREATED":
-      return {
-        ...state,
-        news: [...state.news,action.payload],
-      }
-    case "NEWS_DELETED":{
-      const newNewsList = state.news.filter(s => s.id !== action.payload);
-      return {
-        ...state,
-        news: newNewsList,
-      }
-    }
-    default:
-      return state;
-  }
-}
+
+const news = createReducer(initialState, builder => {
+  builder
+    .addCase(newsFetching,(state) => {state.newsLoadingStatus = 'loading';})
+    .addCase(newsFetched,(state,action) => {
+      state.newsLoadingStatus = 'tohir';
+      state.news = action.payload;
+    })
+    .addCase(newsFetchingError,state => {state.newsLoadingStatus = "error"})
+    .addCase(newsCreated, (state,action) => {state.news.push(action.payload)})
+    .addCase(newsDeleted,(state,action) => {state.news = state.news.filter(s => s.id !== action.payload)})
+    .addDefaultCase(() => {})
+});
 export default news;
+// const news = (state = initialState,action) => {
+//   switch (action.type){
+//     case "NEWS_FETCHING":
+//       return {
+//         ...state,
+//         newsLoadingStatus: 'loading'
+//       }
+//     case "NEWS_FETCHED":
+//       return {
+//         ...state,
+//         news: action.payload,
+//         newsLoadingStatus: "tohir",
+//       }
+//     case "NEWS_FETCHING_ERROR":
+//       return {
+//         ...state,
+//         newsLoadingStatus: 'error'
+//       }
+//     case "NEWS_CREATED":
+//       return {
+//         ...state,
+//         news: [...state.news,action.payload],
+//       }
+//     case "NEWS_DELETED":{
+//       const newNewsList = state.news.filter(s => s.id !== action.payload);
+//       return {
+//         ...state,
+//         news: newNewsList,
+//       }
+//     }
+//     default:
+//       return state;
+//   }
+// }
+// export default news;
